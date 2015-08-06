c9aa9f1f6e053148ff2c705e3f68d815aed970ea
Matt Lucas <a.matt.lucas@gmail.com>
2015-08-05 19:25:05 -0700

    added rake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #neccessary for heroku
+  #needs to be change to domain name. Domain has not been registered. 
 end
